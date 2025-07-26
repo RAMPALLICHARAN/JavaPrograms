de61971232d7b4b72b2551780ae0eed06cae92e4
RAMPALLICHARAN <rampallicharan@gmail.com>
2025-07-26 14:40:35 +0530

    Commit

---
+++ src/Sample_Day7_Programs/Factorial.java
@@ -0,0 +1,25 @@
+package Sample_Day7_Programs;
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 100 ,fact = 1;
+
+        try {
+            if (num < 0) {
+                throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+            }
+
+            for (int i = 1; i <= num; i++) {
+                fact *= i;
+            }
+
+            System.out.println("Factorial of " + num + " is: " + fact);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Input error: " + e.getMessage());
+        } finally {
+            System.out.println("Execution completed (try-catch-finally block finished).");
+        }
+    }
+}
+
+
+
